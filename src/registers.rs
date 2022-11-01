ef0f2034b12de5335569e057a61abede42ab098a
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2022-11-01 19:53:16 -0400

    add all registers to the enum

--- src/registers.rs
+++ src/registers.rs
@@ -3,14 +3,40 @@ pub enum Reg {
     V0,
     V1,
     V2,
+    V3,
+    V4,
+    V5,
+    V6,
+    V7,
+    V8,
+    V9,
+    VA,
+    VB,
+    VC,
+    VD,
+    VE,
+    VF,
 }
 
 impl Into<usize> for Reg {
     fn into(self) -> usize {
         match self {
-            Self::V0 => 0,
-            Self::V1 => 1,
-            Self::V2 => 2,
+            Self::V0 => 0x0,
+            Self::V1 => 0x1,
+            Self::V2 => 0x2,
+            Self::V3 => 0x3,
+            Self::V4 => 0x4,
+            Self::V5 => 0x5,
+            Self::V6 => 0x6,
+            Self::V7 => 0x7,
+            Self::V8 => 0x8,
+            Self::V9 => 0x9,
+            Self::VA => 0xA,
+            Self::VB => 0xB,
+            Self::VC => 0xC,
+            Self::VD => 0xD,
+            Self::VE => 0xE,
+            Self::VF => 0xF,
         }
     }
 }
@@ -21,6 +47,19 @@ impl Into<Reg> for u8 {
             0x0 => Reg::V0,
             0x1 => Reg::V1,
             0x2 => Reg::V2,
+            0x3 => Reg::V3,
+            0x4 => Reg::V4,
+            0x5 => Reg::V5,
+            0x6 => Reg::V6,
+            0x7 => Reg::V7,
+            0x8 => Reg::V8,
+            0x9 => Reg::V9,
+            0xA => Reg::VA,
+            0xB => Reg::VB,
+            0xC => Reg::VC,
+            0xD => Reg::VD,
+            0xE => Reg::VE,
+            0xF => Reg::VF,
             _ => panic!("Unknown register V{:x}", self)
         }
     }
